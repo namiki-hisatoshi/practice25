1245811f860cdd87c10e80776c21d88d1758c650
並木　久稔 <namikihisatoshi@namikihisatoshinoMacBook-Pro.local>
2021-01-05 01:50:22 +0900

    ruby_game

--- portfolio/main.rb
+++ portfolio/main.rb
@@ -1,12 +1,11 @@
+require 'pry'
 
 puts <<~TEXT
 
                ＜マルバツゲーム＞
 
               ======ルール======
-  ・player1が「○」、player2が「✖️」を交互に置く
-
-  ・1~9の数字を選択すると該当する数字の場所に○か✖️を置く
+  ・1~9の数字を選択すると該当する数字の場所に○を置く
 
   ・先に縦・横・斜めの１列が揃うと勝利
 
@@ -18,21 +17,81 @@ tables = [
   ["7", "8", "9"]
 ]
 
-tables.each do |table|
-  puts "|#{table[0]}" + "|" + "#{table[1]}" + "|" + "#{table[2]}" + "|"
+# テーブルを表示
+def disp_table(tables)
+  tables.each do |table|
+    puts "|#{table[0]}" + "|" + "#{table[1]}" + "|" + "#{table[2]}" + "|"
+  end
 end
 
-  def action(tables)
-    while true
-      print "番号を選択して下さい > "
-      select_num = gets.to_i
-      break if(1..9).include?(select_num)
-      puts "1~9の番号で選択して下さい。"
-    end
-    if select_num -1.to_s == tables[0[0]] || [0[1]] || [0[2]] || [1[0]] || [1[1]] ||[1[2]] || [2[0]] || [2[1]] || [2[2]]
-      tables[] = "○"
-    end
+# 番号を選択
+def choose_num(tables)
+  while true
+    print "番号を選択して下さい > "
+    select_num = gets.to_i
+    break if(1..9).include?(select_num)
+    puts "1~9の番号で選択して下さい。"
   end
+  tables = tables.flatten
+  tables[select_num - 1] = "○"
+  tables = [tables[0..2],tables[3..5],tables[6..8]]
+  tables.each do |table|
+    puts "|#{table[0]}" + "|" + "#{table[1]}" + "|" + "#{table[2]}" + "|"
+  end
+  tables.map!
+end
 
+# 結果を判定
+def check_decidion(chosen_num)
+  chosen_num = chosen_num.flatten
+  # 横のパターン　３通り
+  # 1-2-3
+  if chosen_num[0, 1, 2] == ["○", "○", "○"]
+    return true
+  # 4-5-6
+  elsif chosen_num[3, 4, 5] == ["○", "○", "○"]
+    return true
+  # 7-8-9
+  elsif chosen_num[6, 7, 8] == ["○", "○", "○"]
+    return true
+  # 縦のパターン　３通り
+  # 1-4-7
+  elsif chosen_num[0, 3, 6] == ["○", "○", "○"]
+    return true
+  # 2-5-8
+  elsif chosen_num[1, 4, 7] == ["○", "○", "○"]
+    return true
+  # 3-6-9
+  elsif chosen_num[2, 5, 8] == ["○", "○", "○"]
+    return true
+  # 斜めのパターン　２通り
+  # 1-5-9
+  elsif chosen_num[0, 4, 8] == ["○", "○", "○"]
+    return true
+  # 3-5-7
+  elsif chosen_num[2, 4, 6] == ["○", "○", "○"]
+    return true
+  else
+    return false
+  end
+end
 
-  action(tables)
+# 繰り返し判定
+def repeat_action(checked)
+  loop{
+    checked == false
+    choose_num(tables)
+    if checked == true
+      break
+      puts "揃いました！"
+    end
+   }
+end
+
+disp_table(tables)
+
+chosen_num = choose_num(tables)
+
+checked = check_decidion(chosen_num)
+
+repeat_action(checked)
